f74ea17fdbc530cf9f83f9166e1606ed4fd610d8
Fernando Castillo <superferenabled@gmail.com>
2025-11-26 22:00:46 -0600

    changes in router

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter, Navigate} from 'react-router';
+import {createHashRouter, Navigate} from 'react-router';
 import {HomePage} from "@/heroes/pages/home/HomePage";
 import {HeroPage} from "@/heroes/pages/hero/HeroPage";
 // import {SearchPage} from "@/heroes/pages/search/SearchPage";
@@ -9,7 +9,8 @@ import {lazy} from "react";
 
 const SearchPage = lazy(() => import("@/heroes/pages/search/SearchPage"));
 
-export const appRouter = createBrowserRouter([
+// export const appRouter = createBrowserRouter([
+export const appRouter = createHashRouter([
 
     {
         path: '/',
